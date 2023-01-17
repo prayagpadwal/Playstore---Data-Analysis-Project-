eea454db3794a99f67bb9d5b8f30f28a7748e8af
Prayag Padwal <65147413+prayagpadwal@users.noreply.github.com>
2023-01-16 23:45:50 -0800

    Add files via upload

--- Edit.py
+++ Edit.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from numpy import nan
 
+df = pd.DataFrame
+
 # Read file
 file = pd.read_csv(r"C:\.PRAYAG\LiveProject_LinkedIn\playstore_apps.csv",index_col='App')
 
@@ -15,6 +17,12 @@ print(file[file['Category']=='1.9'])
 file.drop("Life Made WI-Fi Touchscreen Photo Frame", inplace=True)
 print(file['Category'].unique()) # Check if irrelevant values are removed
 
-#Export the cleaned dataset as a .csv file and prepare the data using excel.
 
-file.to_csv("cleaned_file.csv")
+# Remove 'M' from size column
+# df['Size'] = df['Size'].astype(str).str[:-2].astype(np.int64)
+
+# Remove all the cells having NULL values
+df = df.dropna(how='any',axis=0)
+
+#Export the cleaned dataset as a .csv file and prepare the data using excel.
+file.to_csv("C:\.PRAYAG\LiveProject_LinkedIn\Folder Edits\cleaned_file.csv")
